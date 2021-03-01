11dad388cdcc132fb908a57502edd042fbc45d95
Oleg Novosad <adminnovik@gmail.com>
2021-03-01 20:31:06 +0200

    #6 Renamed test alias

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,10 +1,10 @@
-import 'src/google_my_business_test.dart' as gmbApiTest;
+import 'src/google_my_business_test.dart' as googleMyBusinessTest;
 import 'src/managers/locations_manager_test.dart' as locationsManagerTest;
 import 'src/managers/reviews_manager_test.dart' as reviewsManagerTest;
 
 void main() {
   // Main
-  gmbApiTest.main();
+  googleMyBusinessTest.main();
 
   // Managers
   locationsManagerTest.main();
