77aaaa0583fc1316455a5e5be393e13c92a4b8b0
Oleg Novosad <adminnovik@gmail.com>
2021-03-08 13:51:17 +0200

    #13 Added documentation to reviews model classes

--- lib/src/models/review/review_reply.dart
+++ lib/src/models/review/review_reply.dart
@@ -2,9 +2,16 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'review_reply.g.dart';
 
+/// Represents the location owner/manager's reply to a review.
 @JsonSerializable()
 class ReviewReply {
+  /// The body of the reply as plain text with markups. The maximum length is 4096 bytes.
   final String? comment;
+
+  /// Output only. The timestamp for when the reply was last modified.
+  ///
+  /// A timestamp in RFC3339 UTC "Zulu" format, with nanosecond resolution and up to nine fractional digits.
+  /// Examples: `"2014-10-02T15:01:23Z"` and `"2014-10-02T15:01:23.045123456Z"`.
   final String? updateTime;
 
   ReviewReply(this.comment, this.updateTime);
--- lib/src/models/review/reviewer.dart
+++ lib/src/models/review/reviewer.dart
@@ -2,10 +2,16 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'reviewer.g.dart';
 
+/// Represents the author of the review.
 @JsonSerializable()
 class Reviewer {
+  /// The profile photo link of the reviewer. Only populated if `isAnonymous` is false.
   final String? profilePhotoUrl;
+
+  /// The name of the reviewer. Only populated with the reviewer's real name if `isAnonymous` is false.
   final String? displayName;
+
+  /// Indicates whether the reviewer has opted to remain anonymous.
   final bool? isAnonymous;
 
   Reviewer(this.profilePhotoUrl, this.displayName, this.isAnonymous);
